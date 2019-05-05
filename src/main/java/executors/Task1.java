7c0cbf970555d81257945cec3e1794d6a3d6141a
Madave94 <david.tschirschwitz@uni-weimar.de>
2019-05-05 10:22:10 +0200

    added mockup and old test cases

--- src/main/java/executors/Task1.java
+++ src/main/java/executors/Task1.java
@@ -2,4 +2,14 @@ package executors;
 
 public class Task1 {
 
+    /*
+     * Create a method which computes the sum of all elements in a 2D integer array.
+     * For better Performance use at least 2 Threads or Runnables in parallel. (Hint:
+     * Have a look on how the join()-Method is working)
+     *
+     */
+
+    public int quickSum(int[] arr) {
+        return 0;
+    }
 }
